5ccfbe5124cb5bd2e4a8e36cd84c44bc508daca0
solasantiago <santiagoms.ss@gmail.com>
2018-02-25 22:14:35 -0300

    Cabecera para Stack contiguo

---
+++ Stack/Contigua/Stack.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+struct Stack {
+	array <int, 10> mystack;
+	int i=0;
+};
+
+void Push(Stack &, int);
+void Pop (Stack &, int &);
+void Top (Stack, int &);
+void CoutStack (Stack &);
+
+void Push (Stack &s, int a)
+{
+	s.mystack.at(s.i) = a;
+	s.i++;
+}
+
+void CoutStack (Stack &s)
+{
+	if (s.i != 0)
+		for (int j = 0 ; j < s.i ; j++)
+			cout << s.mystack.at(j) << " ";
+	
+	else
+		cout << "No hay valores en stack";
+	cout << endl;
+}
+
+void Pop(Stack &s, int &a)
+{
+	if (s.i != 0)
+	{
+		s.i--;
+		a = s.mystack.at(s.i);
+	}
+	
+	else
+		cout << "Empty stack, Pop function cancelled";
+}
+
+void Top(Stack s, int &a)
+{
+	if (s.i != 0)
+		a = s.mystack.at(s.i-1);
+
+	else
+		cout << "Empty stack, Top function cancelled";
+}
