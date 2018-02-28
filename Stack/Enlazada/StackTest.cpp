e13da1decbaf3be5a5061ad006102ee0e7214b5d
solasantiago <santiago.sola@mercadolibre.com>
2018-02-28 19:25:37 -0300

    Create StackTest.cpp

---
+++ Stack/Enlazada/StackTest.cpp
@@ -0,0 +1,31 @@
+#include "Stack.h"
+#include <cassert>
+
+void PushOddDigits (Stack &);
+
+int main ()
+{
+	int Digit;
+	Stack OddDigits;
+	assert ( !Pop (OddDigits, Digit) );
+	PushOddDigits (OddDigits);
+	Pop(OddDigits, Digit);
+	assert ( Digit == 9 );
+	Pop(OddDigits, Digit);
+	assert ( Digit == 7 );
+	Pop(OddDigits, Digit);
+	assert ( Digit == 5 );
+	Pop(OddDigits, Digit);
+	assert ( Digit == 3 );
+	Pop(OddDigits, Digit);
+	assert ( Digit == 1 );
+	cout << "Test completed successfully";
+}
+
+void PushOddDigits (Stack &OddDigits)
+{
+	for (int i=1; i<10; i=i+2)
+	{
+		Push(OddDigits, i);
+	}
+}
