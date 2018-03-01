5f7f989a7b8c83a72f9a701c0d7bdfc2dcf12c7b
solasantiago <santiago.sola@mercadolibre.com>
2018-02-28 23:46:41 -0300

    Update Queue.h

--- Queue/Contigua/Queue.h
+++ Queue/Contigua/Queue.h
@@ -1,1 +1,55 @@
+#include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Enqueue (Queue &s, int a)
+{
+	if ( s.i < s,j and s.j!=100 )	//	El indicador de inicio está por delante del indicador de final
+	{
+		s.myqueue.at(s.j) = a;
+		s.j++;
+	}
+	
+	else if ( s.i-s.j < 0 )
+	{
+		s.myqueue.at(s.j) = a;
+		s.j++;
+	}
+	
+	else if (s.j==100)
+	{
+		s.myqueue.at(0) = a;
+		s.j=1;
+	}
+
+	else			//	El indicador de inicio está por delante del indicador de final
+	{
+		cout << "Error";
+	}
+}
+
+bool Dequeue (Queue &s, int &a)
+{
+	if (s.i < s.j)
+	{
+		a = s.myqueue.at(s.i);
+		s.i++;
+		return true;
+	}
+
+	else if (s.j < s.i and s.i-s.j < 0)
+	{
+		a = s.myqueue.at(s.i);
+		s.i++;
+	}
+	
+	else if (s.i==100)
+	{
+		s.myqueue.at(0) = a;
+		s.i=1;
+	}
+	else
+		return false;
+}
