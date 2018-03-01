78d89bb2a83b50252c2f04be4d431ae31ecdee00
solasantiago <santiago.sola@mercadolibre.com>
2018-02-28 23:05:01 -0300

    Update Queue.h

--- Queue/Enlazada/Queue.h
+++ Queue/Enlazada/Queue.h
@@ -1,1 +1,36 @@
+#include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Enqueue (Queue &s, int a)
+{
+	if (s.FirstNode != nullptr)
+	{
+		Node *aux = s.FirstNode;
+		while (aux->Next != nullptr)
+			aux = aux->Next;
+		aux->Next = new Node;
+		aux->Next->Value = a;
+	}
+
+	else
+	{
+		s.FirstNode = new Node;
+		s.FirstNode->Value = a;
+	}	
+}
+
+bool Dequeue (Queue &s, int &a)
+{
+	if (s.FirstNode != nullptr)
+	{
+		a = s.FirstNode->Value;
+		s.FirstNode = s.FirstNode->Next;
+		return true;
+	}
+
+	else
+		return false;
+}
