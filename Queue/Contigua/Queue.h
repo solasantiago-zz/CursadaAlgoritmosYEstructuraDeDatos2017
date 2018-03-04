84a1152c45c019ffb4e5f0d399f7f0282324ded9
solasantiago <santiago.sola@mercadolibre.com>
2018-03-04 20:18:28 -0300

    Update Queue.h

--- Queue/Contigua/Queue.h
+++ Queue/Contigua/Queue.h
@@ -1,55 +1,19 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <array>
 #include <iostream>
 
-using std::cout;
-using std::cin;
-using std::endl;
+const int N = 100;
 
-void Enqueue (Queue &s, int a)
-{
-	if ( s.i < s,j and s.j!=100 )	//	El indicador de inicio está por delante del indicador de final
-	{
-		s.myqueue.at(s.j) = a;
-		s.j++;
-	}
-	
-	else if ( s.i-s.j < 0 )
-	{
-		s.myqueue.at(s.j) = a;
-		s.j++;
-	}
-	
-	else if (s.j==100)
-	{
-		s.myqueue.at(0) = a;
-		s.j=1;
-	}
+using std::array;
 
-	else			//	El indicador de inicio está por delante del indicador de final
-	{
-		cout << "Error";
-	}
-}
+struct Queue {
+	array <int, N> myqueue;
+	int i=0, j=0;
+};
 
-bool Dequeue (Queue &s, int &a)
-{
-	if (s.i < s.j)
-	{
-		a = s.myqueue.at(s.i);
-		s.i++;
-		return true;
-	}
+void Enqueue (Queue &, int);
+bool Dequeue (Queue &, int &);
 
-	else if (s.j < s.i and s.i-s.j < 0)
-	{
-		a = s.myqueue.at(s.i);
-		s.i++;
-	}
-	
-	else if (s.i==100)
-	{
-		s.myqueue.at(0) = a;
-		s.i=1;
-	}
-	else
-		return false;
-}
+#endif
