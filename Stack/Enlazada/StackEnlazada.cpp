48080c5a0dcd6dad02d67f2a0293394910b0e3c6
solasantiago <santiago.sola@mercadolibre.com>
2018-03-04 18:40:52 -0300

    Update StackEnlazada.cpp

--- Stack/Enlazada/StackEnlazada.cpp
+++ Stack/Enlazada/StackEnlazada.cpp
@@ -1,14 +1,52 @@
-#include <iostream>
-
-struct Node {
-	int Value;
-	Node *Next = nullptr;
-};
-	
-struct Stack {
-	Node *FirstNode= nullptr;
-};
-
+#include "Stack.h"
 
 void Push(Stack &, int);
 bool Pop (Stack &, int &);
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Push (Stack &s, int a)
+{
+	if (s.FirstNode != nullptr)
+	{
+		Node *aux = s.FirstNode;
+		while (aux->Next != nullptr)
+			aux = aux->Next;
+		aux->Next = new Node;
+		aux->Next->Value = a;
+	}
+
+	else
+	{
+		s.FirstNode = new Node;
+		s.FirstNode->Value = a;
+	}	
+}
+
+bool Pop (Stack &s, int &a)
+{
+	if (s.FirstNode != nullptr)
+	{
+		if (s.FirstNode->Next != nullptr)
+		{
+			Node *aux = s.FirstNode;
+			while (aux->Next->Next != nullptr)
+				aux = aux->Next;
+			a = aux->Next->Value;
+			aux->Next = nullptr;
+		}
+		
+		else
+		{
+			a = s.FirstNode->Value;
+			s.FirstNode = nullptr;
+		}
+		
+		return true;
+	}
+
+	else
+		return false;
+}
