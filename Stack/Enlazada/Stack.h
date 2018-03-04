2d222f7e362a3db5652be71e2d674b3df2e25229
solasantiago <santiago.sola@mercadolibre.com>
2018-03-04 18:40:32 -0300

    Update Stack.h

--- Stack/Enlazada/Stack.h
+++ Stack/Enlazada/Stack.h
@@ -1,49 +1,15 @@
+#ifndef STACK_H
+#define STACK_H
+
 #include <iostream>
 
-using std::cout;
-using std::cin;
-using std::endl;
+struct Node {
+	int Value;
+	Node *Next = nullptr;
+};
+	
+struct Stack {
+	Node *FirstNode= nullptr;
+};
 
-void Push (Stack &s, int a)
-{
-	if (s.FirstNode != nullptr)
-	{
-		Node *aux = s.FirstNode;
-		while (aux->Next != nullptr)
-			aux = aux->Next;
-		aux->Next = new Node;
-		aux->Next->Value = a;
-	}
-
-	else
-	{
-		s.FirstNode = new Node;
-		s.FirstNode->Value = a;
-	}	
-}
-
-bool Pop (Stack &s, int &a)
-{
-	if (s.FirstNode != nullptr)
-	{
-		if (s.FirstNode->Next != nullptr)
-		{
-			Node *aux = s.FirstNode;
-			while (aux->Next->Next != nullptr)
-				aux = aux->Next;
-			a = aux->Next->Value;
-			aux->Next = nullptr;
-		}
-		
-		else
-		{
-			a = s.FirstNode->Value;
-			s.FirstNode = nullptr;
-		}
-		
-		return true;
-	}
-
-	else
-		return false;
-}
+#endif
