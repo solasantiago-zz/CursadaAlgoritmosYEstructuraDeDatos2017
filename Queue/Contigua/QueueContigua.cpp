681d5d28568d0dd6ea35bdb193a2f3e6f1ecc50a
solasantiago <santiago.sola@mercadolibre.com>
2018-03-05 20:36:09 -0300

    Update QueueContigua.cpp

--- Queue/Contigua/QueueContigua.cpp
+++ Queue/Contigua/QueueContigua.cpp
@@ -5,46 +5,19 @@ using std::endl;
 
 void Enqueue (Queue &s, int a)
 {
-	if (s.M == N)					// Cola llena
-	{
-		cout << "Error";
-	}
-
-	else if ( s.j == N )		// Esto condiciona a que J empiece a estar por detrás de I
-	{
-		s.myqueue.at(0) = a;
-		s.j = 1;
-		s.M++;
-	}
-	
-	else						//	El indicador de inicio está por delante del indicador de final
-	{
-		s.myqueue.at(s.j) = a;
-		s.j++;
-		s.M++;
-	}
+	s.myqueue.at(s.j%N) = a;
+	s.j++;
 }
 
 bool Dequeue (Queue &s, int &a)
 {
-	if ( s.M == 0 )
+	if ( s.i != s.j )
 	{
-		return false;
-	}
-	
-	else if ( s.i == N )
-	{
-		a = s.myqueue.at(0);
-		s.i = 0;
-		s.M--;
+		a = s.myqueue.at(s.i%N);
+		s.i++;
 		return true;
 	}
 	
 	else
-	{
-		a = s.myqueue.at(s.i);
-		s.i++;
-		s.M--;
-		return true;
-	}
+		return false;
 }
